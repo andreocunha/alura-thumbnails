ba7d2da7ac66f5a88aeb09b0a3b1a655aa950fa3
andreocunha <andreoliveiracunha20@gmail.com>
2022-12-29 09:36:26 -0300

    nova lib

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,25 @@
 import html2canvas from 'html2canvas';
 import { saveAs } from 'file-saver';
 // import save-html-as-image as any typescript
-const { saveAsPng } = require('save-html-as-image');
+// const { saveAsPng } = require('save-html-as-image');
+import { toPng } from 'html-to-image';
 
 export async function handleDownload(title: string, id: string){
   if (!document || !id) return;
 
   if(id == '#thumbnail-alura-mais'){
     const node = document?.getElementById('thumbnail-alura-mais')!;
-    await saveAsPng(node, { filename: `${title}`, printDate: false });
+
+    await toPng(node, { cacheBust: true, })
+      .then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = `${title}.png`;
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
     return
   }
 
