29ca2a5b7e73b6300a90e911f6dc58eb511ab193
andreocunha <andreoliveiracunha20@gmail.com>
2023-01-23 12:04:45 -0300

    reademe page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+const removeImports = require('next-remove-imports')();
+module.exports = removeImports({});
---
+++ src/pages/readme.tsx
@@ -0,0 +1,41 @@
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import styles from '../styles/Readme.module.css'
+import FileSaver from 'file-saver';
+
+import { exampleReadme } from "../mocks/readme";
+
+const MDEditor = dynamic(
+  () => import("@uiw/react-md-editor"),
+  { ssr: false }
+);
+
+export default function Readme() {
+  const [value, setValue] = useState<string | undefined>(exampleReadme);
+
+  async function downloadReadme() {
+    const file = new Blob([value!], {type: 'text/plain;charset=utf-8'});
+    FileSaver.saveAs(file, 'README.md');
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-color-mode', 'dark')
+  }, [])
+
+  return (
+    <div className={styles.container} data-color-mode="dark">
+      <MDEditor 
+        value={value} 
+        onChange={setValue} 
+        height={600}
+        enableScroll={false}
+        style={{
+          width: '80%',
+        }}
+      />
+      <button className={styles.downloadButton} onClick={downloadReadme}>Download</button>
+    </div>
+  );
+}
